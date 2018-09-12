7fe09f330d480eed2c116dcbb9b46f66d759345b
Saraw <Sarawoot@gmail.com>
2018-09-12 17:12:55 +0700

    FirstCommit

---
+++ Sarawoot/Controllers/SarawController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sarawoot.Controllers
+{
+    public class SarawController : Controller
+    {
+        // GET: Saraw
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
