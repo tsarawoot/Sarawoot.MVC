441e8c0743bc771b57394384780b00c7009b1056
Saraw <t.sarawoot@gmail.com>
2018-09-13 01:04:34 +0700

    add simple[fix] model data

--- Sarawoot/Controllers/SarawController.cs
+++ Sarawoot/Controllers/SarawController.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Sarawoot.Models;
+
 namespace Sarawoot.Controllers
 {
     public class SarawController : Controller
     {
+
         // GET: Saraw
         public ActionResult Index()
         {
-            string[] arrayData = new string[] {"Name", "Age", "HiHeightgh", "weight" };
+            string[] arrayData = new string[] { "Name", "Age", "Height", "weight" };
+            //-------------
+            List<HumanModel> humanData = new List<HumanModel>();
+            humanData.Add(new HumanModel());
+            humanData[0].human = new string[4] { "Name", "Age", "Height", "weight" };
+            humanData.Add(new HumanModel());
+            humanData[1].human = new string[4] { "Sarawoot", "34", "175", "66" };
+            humanData.Add(new HumanModel());
+            humanData[2].human = new string[4] { "Budsaba", "32", "150", "40" };
+            humanData.Add(new HumanModel());
+            humanData[3].human = new string[4] { "Sarawoot", "34", "175", "66" };
 
             ViewData["arrayData"] = arrayData;
+            ViewData["humanData"] = humanData;
             return View();
         }
 
---
+++ Sarawoot/Models/HumanData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sarawoot.Models
+{
+    public class HumanModel
+    {
+        public string[] human;// = new string[4];
+    }
+}
